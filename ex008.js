0d79bfdd3e546f72467d8d28e4c985c9efe951c5
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-08-04 16:18:30 -0300

    novos exercícios

---
+++ ex008.js
@@ -0,0 +1,9 @@
+var vel = 50
+
+console.log(`A velocidade é ${vel}`)
+
+if ( vel > 60){
+    console.log(`Você ultrapassou a velocidade permmitida e foi MULTADO!`)
+} 
+
+console.log(`Dirija com cuidado e use o cinto de segurança!`)
