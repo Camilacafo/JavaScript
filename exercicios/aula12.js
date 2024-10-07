66b2c60db542de70fb1ee647c4885ddd40539395
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-10-07 14:16:52 -0300

    novo exercício

--- exercicios/aula12.js
+++ exercicios/aula12.js
@@ -1,12 +1,10 @@
-var idade = 22
-
+var idade = 65
+console.log(`Sua idade é ${idade}`)
 if(idade < 16){
-    console.log('não vota')
-} else if(idade >= 16 && idade < 18){
-     console.log('voto opcional') 
-      
-    } else if(){
-        console.log
-    }
-    
+    console.log('Você não vota!')
+} else if(idade >= 16 && idade < 18 || idade > 65 ){
+    console.log('O seu voto é opcional!')    
+} else{
+    console.log(`O seu Voto é Obrigatório!`)
 }
+    
