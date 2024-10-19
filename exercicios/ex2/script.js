828511a5f1de88d8ca9dc54318f1d7638e160fe4
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-10-19 14:27:16 -0300

    Atualizações do exercício

--- exercicios/ex2/script.js
+++ exercicios/ex2/script.js
@@ -7,6 +7,35 @@ function verificar() {
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('verifique os dados e tente novamente!')
     } else{
-        
+        var fsex = document.getElementsByName('radsexo')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade>=0 && idade<10) {
+                //criança
+            }else if (idade>=10 && idade<21) {
+                //jovem
+            }else if (idade>=21 && idade<50) {
+                // Adulto
+            }else{
+                //idoso
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade>=0 && idade<10) {
+                //criança
+            }else if (idade>=10 && idade<21) {
+                //jovem
+            }else if (idade>=21 && idade<50) {
+                // Adulta
+            }else{
+                //idosa
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     }
 }
