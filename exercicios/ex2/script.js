cfd3c0d5023389fd529c9d3e819fc881d1503fa4
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-10-25 15:31:44 -0300

    Att

--- exercicios/ex2/script.js
+++ exercicios/ex2/script.js
@@ -16,26 +16,35 @@ function verificar() {
             genero = 'Homem'
             if (idade>=0 && idade<10) {
                 //criança
+                img.setAttribute('src', 'imagens/menino.jpg')
             }else if (idade>=10 && idade<21) {
                 //jovem
+                img.setAttribute('src', 'imagens/jovem-menino.jpg')
             }else if (idade>=21 && idade<50) {
                 // Adulto
+                img.setAttribute('src', 'imagens/homem-adulto.jpg')
             }else{
                 //idoso
+                img.setAttribute('src', 'imagens/idoso.jpg')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade>=0 && idade<10) {
                 //criança
+                img.setAttribute('src', 'imagens/menina.jpg')
             }else if (idade>=10 && idade<21) {
                 //jovem
+                img.setAttribute('src', 'imagens/jovem-menina.jpg')
             }else if (idade>=21 && idade<50) {
                 // Adulta
+                img.setAttribute('src', 'imagens/mulher-madura.jpg')
             }else{
                 //idosa
+                img.setAttribute('src', 'imagens/idosa.jpg')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
