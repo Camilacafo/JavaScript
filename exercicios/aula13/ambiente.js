0e6f33b2263a6281816caa8e4f28e9ce446e2938
camila <166883516+Camilacafo@users.noreply.github.com>
2024-10-27 12:48:11 -0300

    exercício

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+
+
+while (c <= 3) {
+ console.log(`passo: ${c}`) 
+ c++  // significa c = c+1
+}
+ console.log('Fim!')
+
+
+ 
---
+++ exercicios/aula13/node_85c00c95fb4cd.tmp.js
@@ -0,0 +1,15 @@
+var c = 1
+
+
+while (c <= 3) {
+ console.log(`passo: ${c}`) 
+ c++  // significa c = c+1
+
+}
+ console.log('Fim!')
+
+ var b = 1
+ do {
+    console.log(`passo: ${b}`) 
+ } while (b <= 3);
+ 
