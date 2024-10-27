4e7b56ad2d07bd57aef1c83561cb844407b36daf
camila <166883516+Camilacafo@users.noreply.github.com>
2024-10-27 17:12:47 -0300

    exercícios

---
+++ exercicios/aula14/aula14.js
@@ -0,0 +1,5 @@
+console.log('Início.')
+for (let c = 1; c <= 5; c++) {
+    console.log(`passo: ${c}`) 
+}
+console.log('Fim!')
---
+++ exercicios/desafio16/script.js
@@ -0,0 +1,10 @@
+function soma() {
+    var valor1 = window.document.getElementById('inicio')
+    var valor2 = window.document.getElementById('final')
+    var valor3 = window.document.getElementById('passo')
+
+    for ( var inicio = valor1 ; valor2 <= valor1 ; inicio = valor3  ) {
+        
+        
+    }
+}
