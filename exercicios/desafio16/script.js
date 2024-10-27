19425703bd04e7127f5087ee71e400d528a9c506
camila <166883516+Camilacafo@users.noreply.github.com>
2024-10-27 20:04:38 -0300

    Exercício

--- exercicios/desafio16/script.js
+++ exercicios/desafio16/script.js
@@ -1,10 +1,32 @@
-function soma() {
-    var valor1 = window.document.getElementById('inicio')
-    var valor2 = window.document.getElementById('final')
-    var valor3 = window.document.getElementById('passo')
+function somar() {
+    let valor1 = window.document.getElementById('inicio')
+    let valor2 = window.document.getElementById('final')
+    let valor3 = window.document.getElementById('passo')
+    let resultado = window.document.getElementById('res')
 
-    for ( var inicio = valor1 ; valor2 <= valor1 ; inicio = valor3  ) {
-        
-        
+if (valor1.value.length == 0 || valor2.value.length == 0 || valor3.value.length == 0) // se o numero de elementos digitados for igual a 0
+ {
+    window.alert('preencha todos os dados')
+}else{
+    resultado.innerHTML = 'contando: <br>'
+    let i = Number(valor1.value)
+    let f = Number(valor2.value)
+    let p = Number(valor3.value)
+
+if(p <= 0){window.alert('passo inválido, considerando passo 1') 
+    p =1}
+
+if (i<f) {
+    for ( let c = i; c <= f ; c += p ) {
+        res.innerHTML += ` ${c} \u{1F449}`   
     }
+    res.innerHTML += ` Fim \u{1f3c1}`
+} else{
+    for (let c = i; c >= f; c -= p) {
+        res.innerHTML += ` ${c} \u{1F449}`  
+    }
+    res.innerHTML += ` Fim \u{1f3c1}`
+}
+}    
+    
 }
