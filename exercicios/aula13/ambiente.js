f1a8c722f1c2acde0cd01ee8e2047206da059cc4
camila <166883516+Camilacafo@users.noreply.github.com>
2024-10-27 12:54:09 -0300

    exercício

--- exercicios/aula13/ambiente.js
+++ exercicios/aula13/ambiente.js
@@ -1,11 +1,16 @@
-var c = 1
-
-
+/*var c = 1
 while (c <= 3) {
  console.log(`passo: ${c}`) 
  c++  // significa c = c+1
 }
- console.log('Fim!')
+ console.log('Fim!')*/
+
+ var c =1
+ do {
+    console.log(`passo: ${c}`) 
+    c++  // significa c = c+1
+ } while (c<=3)
+console.log('Fim!');
 
 
  
