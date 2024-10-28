ae97fa9fe4148bc86dcfaf7ca7147800ee6c4b5a
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-10-28 14:43:45 -0300

    Tabuada

--- exercicios/desafio17/script.js
+++ exercicios/desafio17/script.js
@@ -1,1 +1,20 @@
-alert(`Olá!`)
+function multiplicar(){
+ let valor1 = document.getElementById('num')
+ let valor2 = document.getElementById('seltab')
+
+ if (valor1.value.length == 0) {
+    window.alert('Digite um número.')
+ }else{
+    let n = Number(valor1.value)  
+    let c = 1
+    valor2.innerHTML = ''
+    while (c <= 10) {
+        let item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        item.value = `valor2${c}`
+        valor2.appendChild(item)
+        c++
+    }
+ }
+ 
+}
